d72325fd8e1bf3bef9df1063b975b4bddbb67662
Kevin <kevin.lucassg@gmail.com>
2024-03-16 23:50:03 -0300

    More information

--- src/Components/NewsContent.jsx
+++ src/Components/NewsContent.jsx
@@ -15,7 +15,9 @@ const NewsContent = ({ title, description, src, url }) => {
       <div className="card-body">
         <h5 className="card-title">{title.slice(0, 50)}</h5>
         <p className="card-text">
-          {description ? description.slice(0, 90) : "News"}
+          {description
+            ? description.slice(0, 90)
+            : "News current event. It is information about something that has just happened."}
         </p>
         <a href={url} className="btn btn-primary">
           Read More
